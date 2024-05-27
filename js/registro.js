45c863bb1a666da4e26a7c123d19e617b3dd6c6f
NoeGMol <168143019+NoeGMol@users.noreply.github.com>
2024-05-26 21:31:36 -0300

    Add files via upload
    js del registro

---
+++ js/registro.js
@@ -0,0 +1,64 @@
+// HEADER 
+
+let miheader = `
+    
+    
+    <nav>
+    
+    <div class="titulo">
+    <h1> Surf & Stuff Co. </h1>  
+    </div>
+    
+    
+    
+    <div class="contenedornav">
+            <a class="anav" href="index.html">HOME</a>
+            <a class="anav" href="surf.html">SURF</a>
+            <a class="anav" href="contacto.html">CONTACTO</a>
+            <a class="anav" href="quienessomos.html">QUIENES SOMOS</a>
+        </div>
+    </nav>
+
+
+    
+
+`
+document.querySelector("header").innerHTML = miheader;
+
+//FORMULARIO
+
+ 
+
+//FOOTER
+
+
+let miFooter = `
+
+
+    <div class="contendenorfooter">
+    <div>
+    <a class="afooter" href="index.html">Home</a>
+    <a class="afooter" href="surf.html">Surf</a>
+    <a class="afooter" href="contacto.html">Contacto</a>
+    <a class="afooter" href="quienessomos.html">Quienes somos</a>
+
+
+    <br><br>
+    <h3>2024 Codo a Codo. All rights reserved (©).</h3>
+    </div>
+    
+    
+    
+    
+
+    </div>
+
+    
+    
+
+
+
+
+`
+
+document.querySelector("footer").innerHTML = miFooter;
